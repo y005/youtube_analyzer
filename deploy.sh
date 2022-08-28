812910f28e20b74d8c429b367ad7fa4b81c0b5f9
y005 <37391733+y005@users.noreply.github.com>
2022-08-28 11:40:39 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "start docker-compose up: ubuntu"
-sudo docker rm -f $(docker ps -qa)
+sudo docker rm -f $(sudo docker ps -qa)
 sudo docker-compose -f /home/ubuntu/project01/docker-compose.prod.yml up --build -d
