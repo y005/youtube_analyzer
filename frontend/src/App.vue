147f7ea52259b1ce8b5349a62c315b2220ed5638
y005 <y005@naver.com>
2023-06-25 17:47:58 +0900

    뷰 프로젝트 설정

---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+import Header from "./components/Header.vue";
+import Main from "./components/Main.vue";
+</script>
+
+<template>
+    <Header></Header>
+    <Main></Main>
+</template>
+
+<style scoped>
+</style>
---
+++ frontend/vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8080'
+    },
+  },
+  build: {
+    outDir: "../src/main/resources/static",
+    assetsDir: "./asset"
+  }
+})
